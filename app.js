f1bb1380caa393a62018d81113d52c3d06c16f31
Mel Garza <melgarza3016@gmail.com>
2020-03-07 16:55:25 -0700

    got it to post to the html

--- app.js
+++ app.js
@@ -9,131 +9,134 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-var employees = [];
+let employeeArray = [];
 
-inquirer
-  .prompt([
-    {
-      type: "imput",
-      name: "managerName",
-      message: "enter manager name"
-    },
-    {
-      type: "imput",
-      name: "managerID",
-      message: "enter manager ID"
-    },
-    {
-      type: "imput",
-      name: "managerEmail",
-      message: "enter manager email"
-    },
-    {
-      type: "imput",
-      name: "managerOfficenumber",
-      message: "enter manager office number"
-    }
-  ])
-  .then(function(res) {
-    const manager = new Manager(
-      res.managerName,
-      res.managerID,
-      res.managerEmail,
-      res.managerOfficenumber
-    );
-    employees.push(manager);
-  });
+function newEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter employee name",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "Enter empolyee ID",
+        name: "id"
+      },
+      {
+        type: "input",
+        message: "Enter employee email",
+        name: "email",
+        validate: input => {
+          if (input.includes("@") && input[input.length - 4] === ".") {
+            return true;
+          } else {
+            console.log("\n Email not valid, please try again");
+            return false;
+          }
+        }
+      },
+      {
+        type: "list",
+        message: "Choose employee role",
+        name: "role",
+        choices: ["Engineer", "Manager", "Intern"]
+      }
+    ])
+    .then(response => {
+      if (response.role === "Manager") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is the Office Number?",
+              name: "officeNumber"
+            }
+          ])
+          .then(managerData => {
+            let manager = new Manager(
+              response.name,
+              response.id,
+              response.email,
+              managerData.officeNumber
+            );
+            employeeArray.push(manager);
+            console.log(`manager: ${manager.name}`);
+            addEmployee();
+          });
+      } else if (response.role === "Engineer") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Enter your Github Username",
+              name: "github"
+            }
+          ])
+          .then(engineerData => {
+            let engineer = new Engineer(
+              response.name,
+              response.id,
+              response.email,
+              engineerData.github
+            );
+            employeeArray.push(engineer);
+            console.log(
+              `engineer: ${engineer.name}, Github: ${engineer.github}`
+            );
+            addEmployee();
+            console.log(`Employee Array: ${JSON.stringify(employeeArray)}`);
+          });
+      } else if (response.role === "Intern") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What school do you attend?",
+              name: "school"
+            }
+          ])
+          .then(internData => {
+            let intern = new Intern(
+              response.name,
+              response.id,
+              response.email,
+              internData.school
+            );
+            employeeArray.push(intern);
+            console.log(`intern: ${intern.name}`);
+            addEmployee();
+          });
+      }
+    });
+}
+
+newEmployee();
 
 function addEmployee() {
   inquirer
     .prompt([
       {
-        type: "list",
-        name: "manager",
-        message: "choose an employee to add",
-        choices: ["Engineer", "Intern", "exit"]
+        type: "confirm",
+        message: "Would you like to add another Employee?",
+        name: "add"
       }
     ])
-    .then(function(res) {
-      switch (res.manager) {
-        case "Engineer":
-          getEngineerdetails();
-          break;
-        case "Intern":
-          getInterndetails();
-          break;
-        case "exit":
-          process.exit(0);
+    .then(response => {
+      if (response.add === true) {
+        newEmployee();
+      } else {
+        createHTML();
       }
     });
 }
 
-function getEngineerdetails() {
-  inquirer
-    .prompt([
-      {
-        type: "imput",
-        name: "EngineerName",
-        message: "enter engineer name"
-      },
-      {
-        type: "imput",
-        name: "engineerID",
-        message: "enter engineer ID"
-      },
-      {
-        type: "imput",
-        name: "engineerEmail",
-        message: "enter engineer email"
-      },
-      {
-        type: "imput",
-        name: "engineerGithub",
-        message: "enter github user ID"
-      }
-    ])
-    .then(function(res) {
-      const engineer = new Engineer(
-        res.engineerName,
-        res.engineerID,
-        res.engineerEmail,
-        res.engineerGithub
-      );
-      employees.push(engineer);
-    });
-
-  function getInterndetails() {
-    inquirer
-      .prompt([
-        {
-          type: "imput",
-          name: "InternName",
-          message: "enter interm name"
-        },
-        {
-          type: "imput",
-          name: "internID",
-          message: "enter intern ID"
-        },
-        {
-          type: "imput",
-          name: "internEmail",
-          message: "enter intern email"
-        },
-        {
-          type: "imput",
-          name: "internSchoolname",
-          message: "enter school name"
-        }
-      ])
-      .then(function(res) {
-        const intern = new Intern(
-          res.internName,
-          res.internID,
-          res.internEmail,
-          res.internSchoolname
-        );
-        employees.push(intern);
-      });
-  }
+function createHTML() {
+  console.log(`Employee Array: ${JSON.stringify(employeeArray)}`);
+  fs.writeFile(outputPath, render(employeeArray), function(err) {
+    if (err) {
+      throw err;
+    }
+  });
 }
